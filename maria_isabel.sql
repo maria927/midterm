e7d38da932ee70167cc320d0d4e7e71485848bb8
maria927 <36428514+maria927@users.noreply.github.com>
2018-04-25 21:56:53 -0500

    Add files via upload
    Export data

---
+++ maria_isabel.sql
@@ -0,0 +1,20 @@
+--------------------------------------------------------
+-- Archivo creado  - jueves-abril-26-2018   
+--------------------------------------------------------
+--------------------------------------------------------
+--  DDL for Function FUNCTION_1
+--------------------------------------------------------
+
+  CREATE OR REPLACE FUNCTION "EJERCICIOS"."FUNCTION_1" (a IN number, b IN number, c IN number)
+ RETURN VARCHAR2 IS
+resultado VARCHAR2(255);
+BEGIN
+
+IF (b>=c) THEN
+resultado:=a||'/'||a*b||'/'||'andres';
+ELSE
+resultado:=a||'/'||a*b||'/'||'martinez';
+END IF;
+
+RETURN resultado;
+END;
